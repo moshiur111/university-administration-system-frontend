e59aeee0c97569cbb92a79f8e965ead4c0fa2441
moshiur111 <moshiur.code@gmail.com>
2026-04-06 17:12:28 +0600

    refactor(api): update baseApi for course module integration

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -57,6 +57,6 @@ const baseQueryWithRefreshToken: BaseQueryFn<
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: baseQueryWithRefreshToken,
-  tagTypes: ["Student", "SemesterRegistration"],
+  tagTypes: ["Student", "SemesterRegistration", "Course"],
   endpoints: () => ({}),
 });
