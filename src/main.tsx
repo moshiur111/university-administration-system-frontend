bfd126b87d1bd83f169177a22fe7cbb309d18294
moshiur111 <moshiur.code@gmail.com>
2026-04-06 13:46:15 +0600

    refactor(app): update main entry and layout integration

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,15 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
-import { persistor, store } from "./app/store.ts";
+import { Toaster } from "sonner";
+import { persistor, store } from "./redux/store.ts";
 import { router } from "./routes/router.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
+        <Toaster position="bottom-right" richColors expand />
         <RouterProvider router={router} />
       </PersistGate>
     </Provider>
